d32b51baba1570e2e5816ef6b05d8092d4067780
Mike Burns <mgburns@bu.edu>
2014-11-26 12:38:33 -0500

    Comment out branding / layout constants
    Child themes can uncomment as needed, but both are exposed in Customizer now.

--- functions.php
+++ functions.php
@@ -8,10 +8,10 @@
 define( 'RESPONSIVE_THEME_VERSION', '1.0.0' );
 
 // Layout selection
-define( 'BU_RESPONSIVE_LAYOUT', 'default' );
+// define( 'BU_RESPONSIVE_LAYOUT', 'default' );
 
 // Branding
-define( 'BU_BRANDING_TYPE', 'logotype' );
+// define( 'BU_BRANDING_TYPE', 'logotype' );
 
 // Uncomment to use un-minified scripts and styles and enable source maps
 // define( 'SCRIPT_DEBUG', true );
