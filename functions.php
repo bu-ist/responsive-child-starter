b0e68c422c7de9d7915b0d4d03b077f982f01615
ashleykolodziej <kolodz10@bu.edu>
2017-03-30 13:44:54 -0400

    Adds new variables and documentation

--- functions.php
+++ functions.php
@@ -5,11 +5,30 @@
  * @see  https://github.com/bu-ist/responsive-framework/wiki/Starting-a-new-Child-Theme
  */
 
-define( 'RESPONSIVE_THEME_VERSION', '1.0.0' );
+// Your child theme version. Used for cache busting.
+// This is NOT the version of Responsive you are using. This is the version
+// of your theme.
+// See these best practices for release numbers: http://semver.org/
+// 1.0.0 should be reserved for the first time your site goes live.
+define( 'RESPONSIVE_THEME_VERSION', '0.1.0' );
 
 // Layout selection
+// Accepts default, topNav, sideNav, noNav.
 // define( 'BU_RESPONSIVE_LAYOUT', 'default' );
 
-// Branding
+// Branding type
+// You may also force branding parent and title.
+// See https://github.com/bu-ist/responsive-framework/wiki/PHP-Constants#bu_branding_parent
 // define( 'BU_BRANDING_TYPE', 'logotype' );
 
+// Sidebar position
+// Accepts left, right, and bottom. Default is right
+// define( 'BU_RESPONSIVE_SIDEBAR_POSITION', 'right' );
+
+// Sidebar position on single post pages
+// Keeps the sidebar at the bottom of the page on post pages. Default is true
+// define( 'BU_RESPONSIVE_POSTS_SIDEBAR_SHOW_BOTTOM', 'true' );
+
+// Enable or disable script debugging and unminified assets for easier dev.
+// This should be set to false for launch.
+// define( 'SCRIPT_DEBUG', true );
