4da8eaa6fe882a285b5aa7b99902f559847ed100
Todd Milliken <milliktr@gmail.com>
2019-01-28 12:00:37 -0500

    Bring in a module from Foundation
    Brings in the Toggle component from Foundation.
    
    Note that this only works when browserify-shim exists. This could be due to a variety of reasons — such as the Toggle component not being written properly to work with CommonJS modules; or that it was built using browserify-shim since it used jQuery, so browserify-shim must be used in this project as well? More research will be done for this.

---
+++ js-dev/script.js
@@ -0,0 +1,10 @@
+/**
+ * The entry point for theme scripts.
+ *
+ * Modules are imported and compiled into one resulting `script.js` file.
+ *
+ * @package Responsive_Child_Starter
+ */
+
+// Import Foundation scripts.
+import Toggle from 'responsive-foundation';
