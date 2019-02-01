7d8f3fbfddc4d3ab84ec8cff4c6d66bf873ebd40
Alana Martin <alana@stonybrookweb.com>
2019-02-01 13:38:00 -0500

    add class to p tag

--- php-dev/dev-functions.php
+++ php-dev/dev-functions.php
@@ -118,7 +118,7 @@ function dev_sections() {
 	global $hook_messages; // Global OK will only be used in development stage.
 	foreach ( $hooks as $hook => $include ) {
 		$git_url                = 'https://github.com/bu-ist/responsive-framework/search?q=' . $hook;
-		$message                = '<p>';
+		$message                = '<p class ="hook-message">';
 		$message               .= 'This is the location for the ';
 		$message               .= '<a href="' . esc_url( $git_url ) . '">' . $hook . ' hook.</a>';
 		$message               .= '</p>';
