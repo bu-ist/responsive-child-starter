598958b7d2a5156a38262dd80df37c24131080f5
Ashley Kolodziej <kolodz10@bu.edu>
2020-05-07 17:00:29 -0400

    Add JavaScript to watch task

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,7 +1,9 @@
 const { watch, parallel } = require('gulp');
 const { build } = require( './build' );
+const { javascript } = require( './javascript' );
 
 // exports.build = series(clean, parallel(css, javascript));
 exports.watch = function() {
-	watch('css-dev/**/*.scss', { events: 'all' }, build);
+	watch( 'css-dev/**/*.scss', { events: 'all' }, build );
+	watch( 'js-dev/**/*.js', { events: 'all' }, javascript );
 };
